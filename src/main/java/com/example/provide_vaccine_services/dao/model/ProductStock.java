53e8da22a6ee10cae7554a6ddf924e5ddab3c95c
DDuowg <doducduong1007@gmail.com>
2025-05-14 23:37:01 +0700

    thêm các model đầy đủ hơn

---
+++ src/main/java/com/example/provide_vaccine_services/dao/model/ProductStock.java
@@ -0,0 +1,11 @@
+package com.example.provide_vaccine_services.dao.model;
+
+import java.time.LocalDateTime;
+
+public class ProductStock {
+    int warehouseId;
+    int vaccineId;
+    int quantity;
+    LocalDateTime last_updated;
+
+}
